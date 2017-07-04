fc3cca92f0633cdbf4f7d18b588dd42ecacd13f0
Will Epperson <wepperson@client39-10-192-31.eduroam.upc.edu>
2017-07-04 11:06:47 +0200

    phase 2

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,32 +1,13 @@
 CREATE SCHEMA `RateACity` ;
 
-CREATE TABLE `RateACity`.`REGULAR_USER` (
+CREATE TABLE `RateACity`.`USER` (
 `Email` VARCHAR(45) NOT NULL,
 `Password` INT NOT NULL,
 `DateJoined` DATETIME NULL,
-`IsSuspended` TINYINT(1) NULL DEFAULT 0,
+`IsManager` TINYINT(1) NOT NULL DEFAULT 0,
+`IsSuspended` TINYINT(1) NOT NULL DEFAULT 0,
 PRIMARY KEY (`Email`),
 CHECK (Email IN('@', '.')) );
-   
-CREATE TABLE `RateACity`.`MANAGER` (
-`Email` VARCHAR(45) NOT NULL,
-`Password` INT NOT NULL,
-`DateJoined` DATETIME NULL,
-PRIMARY KEY (`Email`),
-CHECK (Email IN('@', '.')) );
-  
-CREATE TABLE `RateACity`.`REVIEW` (
-`UserEmail` VARCHAR(50) NOT NULL,
-`ReviewableEID` INT NOT NULL UNIQUE,
-`Rating` INT NOT NULL,
-`Comment` VARCHAR(45) NOT NULL,
-`CreateDate` DATETIME NOT NULL,
-PRIMARY KEY (`UserEmail`, `REVIEWABLE_EID`),
-FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`REGULAR_USER` (`Email`),
-FOREIGN KEY (`ReviewableEID`) REFERENCES `RateACity`.`REVIEWABLE_ENTITY` (`EntityID`),
-CHECK (Rating>0 AND Rating<6),
-CHECK (UserEmail IN('@', '.')) );
-  
   
 CREATE TABLE `RateACity`.`REVIEWABLE_ENTITY` (
 `EntityID` INT NOT NULL,
@@ -34,17 +15,28 @@ CREATE TABLE `RateACity`.`REVIEWABLE_ENTITY` (
 `UserEmail` VARCHAR(45) NOT NULL,
 `SubmitDate` DATETIME NOT NULL,
 PRIMARY KEY (`EntityID`), 
-FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`REGULAR_USER` (`Email`),
+FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`USER` (`Email`),
 CHECK (UserEmail IN('@', '.')) );
    
+CREATE TABLE `RateACity`.`REVIEW` (
+`UserEmail` VARCHAR(50) NOT NULL,
+`ReviewableEID` INT NOT NULL UNIQUE,
+`Rating` INT NOT NULL,
+`Comment` VARCHAR(45) NOT NULL,
+`CreateDate` DATETIME NOT NULL,
+PRIMARY KEY (`UserEmail`, `ReviewableEID`),
+FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`USER` (`Email`),
+FOREIGN KEY (`ReviewableEID`) REFERENCES `RateACity`.`REVIEWABLE_ENTITY` (`EntityID`),
+CHECK (Rating>0 AND Rating<6),
+CHECK (UserEmail IN('@', '.')) );
    
 CREATE TABLE `RateACity`.`CITY` (
 `CityEID` INT NOT NULL,
 `CityName` VARCHAR(45) NOT NULL,
 `Country` VARCHAR(45) NOT NULL,
 `State` VARCHAR(45) NULL,
-PRIMARY KEY (`EID`),
-FOREIGN KEY (`EID`) REFERENCES `RateACity`.`REVIEWABLE_ENTITY` (`EntityID`));
+PRIMARY KEY (`CityEID`),
+FOREIGN KEY (`CityEID`) REFERENCES `RateACity`.`REVIEWABLE_ENTITY` (`EntityID`));
     
 CREATE TABLE `RateACity`.`ATTRACTION` (
 `AttractionEID` INT NOT NULL,
@@ -70,7 +62,6 @@ CREATE TABLE `RateACity`.`FALLS_UNDER` (
 `AttractionEID` INT NOT NULL,
 `CName` VARCHAR(45) NOT NULL,
 PRIMARY KEY (`AttractionEID`, `CName`),
-INDEX `CatName_idx` (`CName` ASC),
 FOREIGN KEY (`AttractionEID`) REFERENCES `RateACity`.`ATTRACTION` (`AttractionEID`),
 FOREIGN KEY (`CName`) REFERENCES `RateACity`.`CATEGORY` (`CName`) );
 
