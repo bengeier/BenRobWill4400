51f49ccb89c26d6eecb6b10c4b8579fc9d090262
Michael Xiao <Michael Xiao>
2017-07-05 18:36:04 +0200

    seriously please let this be the last one, has Stefan's fixes and our missing table

--- CreateTables.sql
+++ CreateTables.sql
@@ -3,32 +3,29 @@ CREATE SCHEMA `RateACity` ;
 CREATE TABLE `RateACity`.`USER` (
 `Email` VARCHAR(45),
 `Password` INT NOT NULL,
-`DateJoined` DATETIME NULL,
+`DateJoined` DATETIME NOT NULL,
 `IsManager` TINYINT(1) NOT NULL DEFAULT 0,
 `IsSuspended` TINYINT(1) NOT NULL DEFAULT 0,
-PRIMARY KEY (`Email`),
-CHECK (Email IN('@', '.')) );
+PRIMARY KEY (`Email`));
   
 CREATE TABLE `RateACity`.`REVIEWABLE_ENTITY` (
 `EntityID` INT,
 `IsPending` TINYINT(1) NOT NULL DEFAULT 0,
-`UserEmail` VARCHAR(45) NOT NULL UNIQUE,
+`UserEmail` VARCHAR(45) NOT NULL,
 `SubmitDate` DATETIME NOT NULL,
 PRIMARY KEY (`EntityID`), 
-FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`USER` (`Email`) ON UPDATE CASCADE,
-CHECK (UserEmail IN('@', '.')) );
+FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`USER` (`Email`) ON UPDATE CASCADE);
    
 CREATE TABLE `RateACity`.`REVIEW` (
-`UserEmail` VARCHAR(50),
+`UserEmail` VARCHAR(45),
 `ReviewableEID` INT,
 `Rating` INT NOT NULL,
-`Comment` VARCHAR(45) NOT NULL,
+`Comment` VARCHAR(255) NOT NULL,
 `CreateDate` DATETIME NOT NULL,
 PRIMARY KEY (`UserEmail`, `ReviewableEID`),
 FOREIGN KEY (`UserEmail`) REFERENCES `RateACity`.`USER` (`Email`) ON UPDATE CASCADE,
 FOREIGN KEY (`ReviewableEID`) REFERENCES `RateACity`.`REVIEWABLE_ENTITY` (`EntityID`),
-CHECK (Rating>0 AND Rating<6),
-CHECK (UserEmail IN('@', '.')) );
+CHECK (Rating>0 AND Rating<6));
    
 CREATE TABLE `RateACity`.`CITY` (
 `CityEID` INT,
