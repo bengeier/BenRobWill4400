8538f7eecc08429fa2229470a9ea97b61a350afa
Robert Firstman <robfirstman@gmail.com>
2017-07-06 18:47:38 +0200

    updated some ish

--- JavaFX/src/main/java/view/UserView.java
+++ JavaFX/src/main/java/view/UserView.java
@@ -1,6 +1,7 @@
 package main.java.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
@@ -17,6 +18,16 @@ public class UserView {
     public void initialize() {
         logOut.setOnAction((event -> {
             RootView.instance.setCenter(LoginView.instance);
+        }));
+
+        deleteAccount.setOnAction((event -> {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirm Delete Account");
+            alert.setHeaderText("Are You Sure?");
+            alert.setContentText("Deleting your account will get rid of your "
+                + "reviews, pending attractions, and pending cities. This action "
+                + "cannot be undone. Do you wish to proceed?");
+            alert.showAndWait();
         }));
     }
 }
