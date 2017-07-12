36d5e1861f48b8d424e03e7bee320733c91143e3
Michael Xiao <Michael Xiao>
2017-07-12 22:09:17 +0200

    Added NewCityForm and NewCityView

---
+++ JavaFX/src/main/java/view/NewCityView.java
@@ -0,0 +1,27 @@
+package main.java.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Created by Michael Xiao Local on 7/12/2017.
+ */
+
+public class NewCityView{
+    public static BorderPane instance = (BorderPane) FXBuilder.getFXMLView("NewCityForm.fxml");
+
+    @FXML
+    private Button //toSignUp, logIn;
+
+    @FXML
+    public void initialize() {
+        /*toSignUp.setOnAction((event -> {
+            RootView.instance.setCenter(SignUpView.instance);
+        }));
+
+        logIn.setOnAction((event -> {
+            RootView.instance.setCenter(UserView.instance);
+        }));*/
+    }
+}
