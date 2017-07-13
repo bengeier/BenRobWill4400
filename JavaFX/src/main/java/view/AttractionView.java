ab1e41ce119b9a4f8bb03429da9506d94d65f0a0
Will Epperson <wepperson@Wills-MacBook-Pro-2.local>
2017-07-13 21:03:35 +0200

    add attraction view

---
+++ JavaFX/src/main/java/view/AttractionView.java
@@ -0,0 +1,10 @@
+package main.java.view;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Created by wepperson on 7/12/17.
+ */
+public class AttractionView {
+    public static BorderPane instance = (BorderPane) FXBuilder.getFXMLView("AttractionPage.fxml");
+}
