e70a28921689a0a04ce4b574ead67d72f3428a90
Robert Firstman <robfirstman@gmail.com>
2017-07-18 20:41:23 +0200

    added some functions for when suspend/promote/delete user actions are possible

--- JavaFX/src/main/java/view/AllUsersListView.java
+++ JavaFX/src/main/java/view/AllUsersListView.java
@@ -1,6 +1,7 @@
 package main.java.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
@@ -22,5 +23,32 @@ public class AllUsersListView {
         addUser.setOnAction((event -> {
             RootView.instance.setCenter(SignUpView.instance);
         }));
+    }
+
+    private void promptForPromote() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Promote Account");
+        alert.setHeaderText("Are You Sure You Want to Promote?");
+        alert.setContentText("The user will be promoted to site manager and be "
+                + "able to promote others, delete accounts, and approve pending "
+                + " cities and locations");
+        alert.showAndWait();
+    }
+
+    private void promptForSuspsend() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Suspend Account");
+        alert.setHeaderText("Are You Sure You Want to Suspend?");
+        alert.setContentText("The user will be unable to post any further comments.");
+        alert.showAndWait();
+    }
+
+    private void promptForDelete() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Delete Account");
+        alert.setHeaderText("Are You Sure You Want to Delete?");
+        alert.setContentText("This will remove all comments and ratings associated "
+            + " with this user.");
+        alert.showAndWait();
     }
 }
