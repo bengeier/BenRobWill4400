82319c62c470ece9c79af4b0776e59a8224f95d8
Robert Firstman <robfirstman@gmail.com>
2017-07-19 10:35:09 +0200

    added some button logic

--- JavaFX/src/main/java/view/NewAttractionView.java
+++ JavaFX/src/main/java/view/NewAttractionView.java
@@ -3,11 +3,20 @@ package main.java.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 
 /**
  * Created by Rob on 7/18/2017.
  */
 public class NewAttractionView {
     public static BorderPane instance = (BorderPane) FXBuilder.getFXMLView("NewAttractionForm.fxml");
+
+    @FXML
+    Button submit, back;
+
+    @FXML
+    public void initialize() {
+        back.setOnAction((event -> {
+            RootView.instance.setCenter(AllAttractionListView.instance);
+        }));
+    }
 }
