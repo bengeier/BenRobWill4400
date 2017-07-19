90327a0248b97d2266c3c5de85c4b24d35968990
Robert Firstman <robfirstman@gmail.com>
2017-07-19 10:22:17 +0200

    Fixed New Attraction View

--- JavaFX/src/main/java/view/NewAttractionView.java
+++ JavaFX/src/main/java/view/NewAttractionView.java
@@ -2,23 +2,12 @@ package main.java.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 /**
  * Created by Rob on 7/18/2017.
  */
 public class NewAttractionView {
-    public static VBox instance = (VBox) FXBuilder.getFXMLView("NewAttractionForm.fxml");
-
-    @FXML
-    Button back, submit;
-
-    @FXML
-    public void initialize() {
-        back.setOnAction((event -> {
-            RootView.instance.setCenter(AllAttractionListView.instance);
-        }));
-
-
-    }
+    public static BorderPane instance = (BorderPane) FXBuilder.getFXMLView("NewAttractionForm.fxml");
 }
