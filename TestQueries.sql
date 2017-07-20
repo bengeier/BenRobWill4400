75b2df34ed2cd8c8762fb18fbfaff5bcc2e18952
Michael Xiao <michaelxiao16@gmail.com>
2017-07-20 22:43:45 +0200

    some more test queries

--- TestQueries.sql
+++ TestQueries.sql
@@ -1,12 +1,22 @@
-# DROP Schema RateACity
-SELECT (CityName)
+#DROP Schema RateACity;
+
+/*SELECT (CityName)
 FROM RateACity.REVIEWABLE_ENTITY NATURAL JOIN RateACity.CITY
 WHERE IsPending = 0
 ORDER BY CityName DESC;
+*/
 
-#counting number of attractions per city
-SELECT COUNT (Attraction)
-FROM RateACity.Attraction AS E NATURAL JOIN RateACity.CITY
+#average rating for city
+SELECT AVG(Rating)
+FROM RateACity.Review AS E JOIN RateACity.CITY AS S ON E.ReviewableEID = S.CityEID;
+
+#counting number of ratings per city
+SELECT COUNT(*)
+FROM RateACity.Review AS E JOIN RateACity.City AS S ON E.ReviewableEID = S.CITYEID;
+
+#counting number of attractions per city	
+SELECT COUNT(*)
+FROM RateACity.Attraction E JOIN RateACity.CITY S ON E.CityEID = S.CityEID;
 
 
 #INSERT INTO RateACity.CITY (CityEID, CityName, Country)*/
