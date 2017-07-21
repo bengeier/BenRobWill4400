d9bc0a2f4f7e34180dcb9ad5ad4b228bb2ad645a
Michael Xiao <michaelxiao16@gmail.com>
2017-07-21 03:58:22 +0200

    TestQueries solved Cities List query

--- TestQueries.sql
+++ TestQueries.sql
@@ -1,11 +1,21 @@
 #DROP Schema RateACity;
-
-/*SELECT (CityName)
+#SELECT * FROM RateACity.Review AS E JOIN RateACity.City AS S ON E.ReviewableEID = S.CITYEID;
+SELECT * FROM
+/*(SELECT DISTINCT (CityName)
 FROM RateACity.REVIEWABLE_ENTITY NATURAL JOIN RateACity.CITY
 WHERE IsPending = 0
-ORDER BY CityName DESC;
-*/
+ORDER BY CityName DESC) AS T 
+JOIN*/
+(SELECT CityName, AVG(Rating), COUNT(Rating)#, COUNT(AttractionEID)
+FROM RateACity.Review AS E JOIN RateACity.City AS S ON E.ReviewableEID=S.CityEID 
+#JOIN RateACity.Attraction AS A ON A.CITYEID = S.CITYEID
+GROUP BY S.CITYEID) AS T 
+NATURAL JOIN (SELECT CityName, Count(AttractionEID) 
+FROM RateACity.Attraction AS A JOIN RateACity.City AS S ON A.CityEID = S.CityEID
+GROUP BY S.CityEID) AS U
+;
 
+/*
 #average rating for city
 SELECT AVG(Rating)
 FROM RateACity.Review AS E JOIN RateACity.CITY AS S ON E.ReviewableEID = S.CityEID;
