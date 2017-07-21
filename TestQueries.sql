60faca82578ccf3289339ae757a41fb9a74ff364
Michael Xiao <michaelxiao16@gmail.com>
2017-07-21 10:21:43 +0200

    finished AllCityLists query

--- TestQueries.sql
+++ TestQueries.sql
@@ -1,19 +1,21 @@
 #DROP Schema RateACity;
 #SELECT * FROM RateACity.Review AS E JOIN RateACity.City AS S ON E.ReviewableEID = S.CITYEID;
-SELECT * FROM
 /*(SELECT DISTINCT (CityName)
 FROM RateACity.REVIEWABLE_ENTITY NATURAL JOIN RateACity.CITY
 WHERE IsPending = 0
 ORDER BY CityName DESC) AS T 
 JOIN*/
-(SELECT CityName, AVG(Rating), COUNT(Rating)#, COUNT(AttractionEID)
+SELECT City, AvgRating, NumRatings, NumAttractions FROM
+(SELECT * FROM
+(SELECT CityEID, CityName AS City, AVG(Rating) AS AvgRating, COUNT(Rating) AS NumRatings
 FROM RateACity.Review AS E JOIN RateACity.City AS S ON E.ReviewableEID=S.CityEID 
-#JOIN RateACity.Attraction AS A ON A.CITYEID = S.CITYEID
 GROUP BY S.CITYEID) AS T 
-NATURAL JOIN (SELECT CityName, Count(AttractionEID) 
+NATURAL JOIN (SELECT S.CityEID, Count(AttractionEID) as NumAttractions 
 FROM RateACity.Attraction AS A JOIN RateACity.City AS S ON A.CityEID = S.CityEID
 GROUP BY S.CityEID) AS U
-;
+JOIN RateACity.Reviewable_Entity AS R
+WHERE IsPending = 0 AND R.EntityID = CityEID
+ORDER BY City ASC) AS Result;
 
 /*
 #average rating for city
