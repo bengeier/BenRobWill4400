0f7180fb957983822ab31c4f75f9f679b9a9584e
Ben Geier <bengeierl97@gmail.com>
2017-07-23 16:18:07 +0200

    City vs Attraction review detection

--- JavaFX/src/main/java/view/ReviewView.java
+++ JavaFX/src/main/java/view/ReviewView.java
@@ -2,6 +2,7 @@ package main.java.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import main.java.model.CurrentState;
 
@@ -22,7 +23,14 @@ public class ReviewView {
     Button submitReview, back;
 
     @FXML
+    Label title;
+
+    @FXML
     public void initialize() {
+        title.setText(CurrentState.peek().equals("AttractionPage.fxml") ? "New Attraction Review For " +
+                CurrentState.getCurrentAttraction().getAttractionName() : "New City Review For " +
+                CurrentState.getCurrentCity().getCityName());
+
         back.setOnAction((event -> {
             RootView.instance.setCenter(FXBuilder.getFXMLView(CurrentState.pop()));
         }));
