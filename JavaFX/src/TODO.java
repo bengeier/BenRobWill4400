2305adee3e8717f2087ddca0d217da907ef8e41e
Will Epperson <willepp@live.com>
2017-07-25 00:04:45 +0200

    todo

--- JavaFX/src/TODO.java
+++ JavaFX/src/TODO.java
@@ -3,27 +3,24 @@
  */
 public class TODO {
 
-    // TODO Make table columns non editable
+    // HIGH PRIORITY
+    // TODO Error popup for bad log in not working
 
-    // TODO Make info / link columns non-sortable
-
-    // TODO Error popup for bad log in
-
-    // TODO Manager
-
+    // TODO Manager...
         // TODO	Search user
-
         // TODO	Users List
-
-    // TODO Shouldn’t be able to demote himself if last
-
-    // TODO Shouldn’t be able to delete himself if last
-
-    // TODO Log in error not working
-
-    // TODO change link color -- cant see when table row highlighted
+        // TODO Shouldn’t be able to demote himself if last
+        // TODO Shouldn’t be able to delete himself if last
 
     // TODO approve pending city doesnt actually approve
+
+
+    // LOW PRIORITY
+    // TODO change background color / image
+    // TODO Make table columns non editable
+    // TODO Make info / link columns non-sortable
+    // TODO change link color -- cant see when table row highlighted
+
 
 
 }
