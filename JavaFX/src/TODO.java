99fbbc23c2756a472544d1a08edf11d21b3d9add
Will Epperson <willepp@live.com>
2017-07-25 22:04:52 +0200

    update to do list

--- JavaFX/src/TODO.java
+++ JavaFX/src/TODO.java
@@ -8,7 +8,7 @@ public class TODO {
 	//TODO adding a new attraction should be sent to manager for approval
 
 	//TODO users that have already reviewed a city should get an "Edit my Review" button (we need to write mySQL UPDATE)
-		//related: if user is updating review, there should be a "delete" button option
+		//TODO related: if user is updating review, there should be a "delete" button option
 
 	//TODO managers should be able to delete a city
 	//TODO managers should be able to delete an attraction
@@ -19,14 +19,5 @@ public class TODO {
 	//QUESTION: do we want user to be taken to a city/attraction screen after adding that city/attraction, even if it is still pending?
 
 // LOW PRIORITY
-
-    // TODO Make some tables bigger (especially pending attractions)
-    // TODO searched attractions do not always display all categories
-    // i.e. if I search for Outdoors attractions in Atlanta, Georgia Tech is displayed, but its categories only show
-    // "Outdoors" instead of "Outdoors, Good View"
-    // TODO change background color / image
-    // TODO Make table columns non editable
-    // TODO Make info / link columns non-sortable
-
 
 }
