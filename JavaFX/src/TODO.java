a07d13043baa95fdf7c6e4e9f8e416928acef7bd
Michael Xiao <michaelxiao16@gmail.com>
2017-07-25 18:10:42 +0200

    moar TODOs

--- JavaFX/src/TODO.java
+++ JavaFX/src/TODO.java
@@ -11,22 +11,33 @@ public class TODO {
 
     // TODO approve pending city doesn't actually approve
 
-	//TODO all cities list MUST BE sorted alphabetically by city name by default
-	//TODO all attractions list MUST BE sorted alphabetically by location name
-	//TODO Users should not be able submit an empty form, must fill in required fields
+	//TODO all cities list MUST BE sorted alphabetically by city name by default (also need to write mySQL for other Sorts)
+	//TODO all attractions list MUST BE sorted alphabetically by location name (also need to write mySQL for other Sorts)
+	//TODO Users should not be able submit an empty city form, must fill in required fields
 	//TODO adding a new city must insert new review to table, reading data from fields
 	//TODO adding a new attraction should be sent to manager for approval
-	//TODO adding a new attraction should require required fields
 
 	//TODO users that have already reviewed a city should get an "Edit my Review" button (we need to write mySQL UPDATE)
-		related: if user is updating review, there should be a "delete" button option
-	//TODO managers should be able to delete any review they want from a city (mySQL DELETE and delete button to appear is isManager)
+		//related: if user is updating review, there should be a "delete" button option
+	
     
 	//TODO on user's review page, they should be able to EDIT their reviews
 
 	//TODO clicking View All attractions buttons on both user and manager pages for some reason 
-	only gives attractions filtered by Good View
+		//only gives attractions filtered by Good View
 
+	//TODO user that have already reviewed an attraction should be able to update their review from that attraction page 
+		//(button should say edit instead of review) and should be able to delete this review
+
+	//TODO managers should be able to delete a city
+	//TODO managers should be able to delete an attraction
+	//TODO managers should be able to delete any review they want from a city 
+		//(mySQL DELETE and delete button to appear if isManager)
+	//TODO managers should be able to delete any review they want from an attraction
+
+	//TODO Attraction's reviews page should be default sorted high to low (also need to write mySQL for other sorts)
+	
+	//TODO mySQL for category sorts alphabetically, reverse alphabetically, by number of attractions ASC and DESC
 
 // LOW PRIORITY
 
