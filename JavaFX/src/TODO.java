809ecf3780260a5b81f8bda331293f380d0be0ab
Michael Xiao <michaelxiao16@gmail.com>
2017-07-25 17:53:32 +0200

    a lot of major TODOs

--- JavaFX/src/TODO.java
+++ JavaFX/src/TODO.java
@@ -3,7 +3,7 @@
  */
 public class TODO {
 
-    // HIGH PRIORITY
+// HIGH PRIORITY
 
     // TODO Manager...
         // TODO	Search user
@@ -11,15 +11,24 @@ public class TODO {
 
     // TODO approve pending city doesn't actually approve
 
-	//TODO all cities list MUST BE sorted alphabetically by city name
+	//TODO all cities list MUST BE sorted alphabetically by city name by default
 	//TODO all attractions list MUST BE sorted alphabetically by location name
 	//TODO Users should not be able submit an empty form, must fill in required fields
 	//TODO adding a new city must insert new review to table, reading data from fields
 	//TODO adding a new attraction should be sent to manager for approval
 	//TODO adding a new attraction should require required fields
 
+	//TODO users that have already reviewed a city should get an "Edit my Review" button (we need to write mySQL UPDATE)
+		related: if user is updating review, there should be a "delete" button option
+	//TODO managers should be able to delete any review they want from a city (mySQL DELETE and delete button to appear is isManager)
+    
+	//TODO on user's review page, they should be able to EDIT their reviews
 
-    // LOW PRIORITY
+	//TODO clicking View All attractions buttons on both user and manager pages for some reason 
+	only gives attractions filtered by Good View
+
+
+// LOW PRIORITY
 
     // TODO Make some tables bigger (especially pending attractions)
     // TODO searched attractions do not always display all categories
@@ -28,7 +37,7 @@ public class TODO {
     // TODO change background color / image
     // TODO Make table columns non editable
     // TODO Make info / link columns non-sortable
-
+    // TODO 
 
 
 }
