9352026438ed814076bc3c88f6b57f47f8885897
Robert Firstman <robfirstman@gmail.com>
2017-07-25 22:12:19 +0200

    todos

--- JavaFX/src/TODO.java
+++ JavaFX/src/TODO.java
@@ -7,9 +7,6 @@ public class TODO {
 
 	//TODO adding a new attraction should be sent to manager for approval
 
-	//TODO users that have already reviewed a city should get an "Edit my Review" button (we need to write mySQL UPDATE)
-		//related: if user is updating review, there should be a "delete" button option
-
 	//TODO managers should be able to delete a city
 	//TODO managers should be able to delete an attraction
 	//TODO managers should be able to delete any review they want from a city 
@@ -19,16 +16,4 @@ public class TODO {
 	//TODO suspended users should not be able to review attractions from a attractions "all reviews" page 
 		//same goes for not reviewing cities from "all reviews" page
 	//QUESTION: do we want user to be taken to a city/attraction screen after adding that city/attraction, even if it is still pending?
-
-// LOW PRIORITY
-
-    // TODO Make some tables bigger (especially pending attractions)
-    // TODO searched attractions do not always display all categories
-    // i.e. if I search for Outdoors attractions in Atlanta, Georgia Tech is displayed, but its categories only show
-    // "Outdoors" instead of "Outdoors, Good View"
-    // TODO change background color / image
-    // TODO Make table columns non editable
-    // TODO Make info / link columns non-sortable
-
-
 }
