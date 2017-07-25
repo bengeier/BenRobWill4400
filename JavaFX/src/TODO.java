50ac1fb7a4afd624235f602f48c97277355499f3
Michael Xiao <michaelxiao16@gmail.com>
2017-07-25 17:29:30 +0200

    adding more VERY important TODOs, related to adding cities and attractions

--- JavaFX/src/TODO.java
+++ JavaFX/src/TODO.java
@@ -11,9 +11,16 @@ public class TODO {
 
     // TODO approve pending city doesn't actually approve
 
+	//TODO all cities list MUST BE sorted alphabetically by city name
+	//TODO all attractions list MUST BE sorted alphabetically by location name
+	//TODO Users should not be able submit an empty form, must fill in required fields
+	//TODO adding a new city must insert new review to table, reading data from fields
+	//TODO adding a new attraction should be sent to manager for approval
+	//TODO adding a new attraction should require required fields
+
 
     // LOW PRIORITY
-	//TODO all cities list is NOT sorted alphabetically by city name
+
     // TODO Make some tables bigger (especially pending attractions)
     // TODO searched attractions do not always display all categories
     // i.e. if I search for Outdoors attractions in Atlanta, Georgia Tech is displayed, but its categories only show
